4376dc24f0036e449e2a3bb076576ef762a2eceb
hrvojesesar007 <hrvojesesar007@gmail.com>
2024-07-27 19:59:19 +0200

    first commit

---
+++ src/views/LandingView.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="pt-16">
+        <h1 class="text-3xl font-semibold mb-4">Welcome to Trip Harmony</h1>
+        <div class="overflow-hidden shadow sm:rounded-md max-w-sm mx-auto text-left">
+            <div class="bg-white px-4 py-5 sm:p-6">
+                <div class="flex justify-between">
+                    <button 
+                        @click="handleStartDriving"
+                        class="rounded-md border border-transparent bg-black py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-gray-600 focus:outline-none">
+                        Start Driving
+                    </button> 
+                    <button 
+                        @click="handleFindARide"
+                        class="rounded-md border border-transparent bg-black py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-gray-600 focus:outline-none">
+                        Find a Ride
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router'
+import { http } from '@/helpers/http'
+
+const router = useRouter()
+
+const handleStartDriving = () => {
+    http().get('/api/driver')
+        .then((response) => {
+            if(response.data.driver) {
+                router.push({
+                    name: 'standby'
+                })
+            } else {
+                router.push({
+                    name: 'driver'
+                })
+            }
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+}
+
+const handleFindARide = () => {
+    router.push({
+        name: 'location'
+    })
+}
+
+</script>
---
+++ src/views/TripView.vue
@@ -0,0 +1,118 @@
+<template>
+    <div class="pt-16">
+        <h1 class="text-3xl font-semibold mb-4">{{ title }}</h1>
+        <div>
+            <div class="overflow-hidden shadow sm:rounded-md max-w-sm mx-auto text-left">
+                <div class="bg-white px-4 py-5 sm:p-6">
+                    <div>
+                        <GMapMap :zoom="14" :center="location.current.geometry" ref="gMap"
+                            style="width:100%; height: 256px;">
+                            <GMapMarker :position="location.current.geometry" :icon="currentIcon" />
+                            <GMapMarker :position="trip.driver_location" :icon="driverIcon" />
+                        </GMapMap>
+                    </div>
+                </div>
+                <div class="bg-gray-50 px-4 py-3 text-center sm:px-6">
+                    <span class="text-xl text-gray-500 text-sm italic">{{ message }}</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+import { useLocationStore } from '@/stores/location';
+import { useTripStore } from '@/stores/trip';
+import { onMounted, ref } from 'vue';
+import Echo from 'laravel-echo';
+import Pusher from 'pusher-js';
+import { useRouter } from 'vue-router';
+
+const location = useLocationStore()
+const trip = useTripStore()
+const router = useRouter()
+
+const title = ref("Waiting on a driver...")
+const message = ref("Driver not found yet, please wait...")
+
+const gMap = ref(null)
+const gMapObject = ref(null)
+
+const currentIcon = {
+    url: 'https://openmoji.org/data/color/svg/1F920.svg',
+    scaledSize: {
+        width: 40,
+        height: 40
+    }
+}
+
+const driverIcon = {
+    url: 'https://openmoji.org/data/color/svg/1F698.svg',
+    scaledSize: {
+        width: 32,
+        height: 32
+    }
+}
+
+const updateMapBounds = () => {
+    let originPoint = new google.maps.LatLng(location.current.geometry),
+        driverPoint = new google.maps.LatLng(trip.driver_location),
+        latLngBounds = new google.maps.LatLngBounds()
+
+    latLngBounds.extend(originPoint)
+    latLngBounds.extend(driverPoint)
+
+    gMapObject.value.fitBounds(latLngBounds)
+}
+
+
+onMounted(() => {
+    gMap.value.$mapPromise.then((mapObject) => {
+        gMapObject.value = mapObject
+    })
+
+    let echo = new Echo({
+        broadcaster: 'pusher',
+        key: 'mykey',
+        cluster: 'mt1',
+        wsHost: window.location.hostname,
+        wsPort: 6001,
+        forceTLS: false,
+        disableStats: true,
+        enabledTransports: ['ws', 'wss']
+    })
+
+    echo.channel(`passenger_${trip.user_id}`)
+        .listen('TripAccepted', (e) => {
+            trip.$patch(e.trip)
+            title.value = "Your driver is on the way!"
+            message.value = `${e.trip.driver.user.name} is coming in a ${e.trip.driver.year} ${e.trip.driver.color} ${e.trip.driver.make} ${e.trip.driver.model} with a license plate #${e.trip.driver.license_plate}`
+        })
+        .listen('TripLocationUpdated', (e) => {
+            trip.$patch(e.trip)
+            setTimeout(updateMapBounds, 5000)
+        })
+        .listen('TripStarted', (e) => {
+            trip.$patch(e.trip)
+            location.$patch({
+                current: {
+                    geometry: e.trip.destination
+                }
+            })
+            title.value = "You're on your way!"
+            message.value = `You are heading to ${e.trip.destination_name}...`
+        })
+        .listen('TripEnded', (e) => {
+            trip.$patch(e.trip)
+            title.value = "You have arrived!"
+            message.value = `Hope you enjoyed your ride with ${e.trip.driver.user.name}`
+            setTimeout(() => {
+                trip.reset()
+                location.reset()
+                router.push({ name: 'landing' })
+            }, 10000)
+        })
+})
+
+</script>
